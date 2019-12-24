96c531b6d7547ea393362680162b5d10031b190a
cheetu18 <59211491+cheetu18@users.noreply.github.com>
2019-12-24 17:57:22 -0500

    Update prb1.rb

--- prb1.rb
+++ prb1.rb
@@ -1,4 +1,4 @@
 def natural_num(max)
-return (1...max).select {|ele| ele % 3 == 0 || ele % 5 == 0}.reduce(:+)
+return (1...max).select {|ele| ele % 3 == 0 || ele % 5 == 0}.sum
 end
 p natural_num(1000)
